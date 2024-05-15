67551778fe3f7045a13c7f5062c6021ed6731b57
bobby2910 <b.mastacan@gmail.com>
2024-05-15 15:18:26 +0100

    fixed: paths to images

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ id, onCardClick }) => {
 
 	useEffect(() => {
 		const fetchImageData = async () => {
-			const imageUrl = `src/assets/image${id}.png`;
+			const imageUrl = `/memory-game/src/assets/image${id}.png`;
 
 			setCardData({
 				imageUrl: imageUrl,
